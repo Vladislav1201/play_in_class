8d022bb83d3e8db93248a18889f38339f51d7d8d
VladN <netuvladika@gmail.com>
2025-10-24 21:54:34 +0300

    last change

--- vlad.py
+++ vlad.py
@@ -1,8 +1,8 @@
+word = "карп"
+
 def announce_players():
-    players = input("Введите количество игроков: ")
-    if players.isdigit():
-        players_int = int(players)
-        name_players = []
+        players_int = 2
+
         for count in range(1, players_int + 1):
             while True:
                 name = input(f"Введите имя игрока номер {count}: ")
@@ -12,7 +12,32 @@ def announce_players():
                     break
                 else:
                     print("введено что-то неверно")
-    else:
-        print("Введенны неверные данные")
 
+count_first = 0
+count_second = 0
+
+
+def func_luck(x, arr, count):
+    if x in arr:
+        count += 10
+    else:
+        count -= 5
+    if x == word:
+        is_word_true = True
+        print(f"Player wins ")
+    return count
+
+
+announce_players()
+is_word_true = False
+while not is_word_true:
+    a = input('first_name: ')
+    func_luck(a, word, count_first)
+    count_first = func_luck(a, word, count_first)
+    print(f"Your current points: {count_first}")
+
+    b = input('second_name: ')
+    func_luck(b, word, count_second)
+    count_second = func_luck(a, word, count_second)
+    print(f"Your current points: {count_second}")
 
